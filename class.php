aa33b6a4715f3dd53dc4f7003d5acd922fdf1a1c
“Hiroki66” <hiroki19981006@gmail.com>
2021-06-03 14:40:42 +0900

    2021-06-03

--- class.php
+++ class.php
@@ -1,40 +1,33 @@
 <?php
-
-class Car
-{
-    public static $class_name ="Car";
-
-    public function __construct()
-    {
-        print("init");
-        $this->name="";
-    }
-
-    public function show()
-    {
-        print($this->name."\n");
-    }
-}
-
-$car = new Car();
-$car_t = new Car();
-$car->name="セダン";
-$car_t->name="12345";
-$car->show();
-$car_t->show();
-print(Car::$class_name);
-
 class Human
 {
-    public static $class_name = "Human";
-
-    public function init()
+    public function __construct()
     {
-        $this->name = "大泉";
+        $this->name = null;
+        $this->address = null;
     }
 
     public function show()
     {
         print($this->name."\n");
+        print($this->address."\n");
     }
 }
+
+class Actor extends Human
+{
+    public function getShow()
+    {
+        parent::show();
+    }
+}
+
+$actor = new Actor();
+
+$actor->name = "大泉";
+$actor->address = "北海道";
+
+// $actor->show();
+$actor->getShow();
+# => 大泉
+#    北海道
--- restaurants_searcher.php
+++ restaurants_searcher.php
@@ -1,21 +1,38 @@
 <?php
-function write_data_to_csv()
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+$KEYID = "メールに記載されていたアクセスキーをここに入力";
+$HIT_PER_PAGE = 100;
+$PREF = "Z011";
+$FREEWORD = "渋谷駅";
+$FORMAT = "json";
+
+$PARAMS = array("keyid"=> $KEYID, "count"=>$HIT_PER_PAGE, "pref"=>$PREF, "keyword"=>$FREEWORD, "format"=>$FORMAT);
+
+function write_data_to_csv($params)
 {
     $restaurants = [];
-    $response = "hugahuga";
-
-    if (isset($response["results"]["error"])) {
-        return print("エラーが発生しました！");
+    $client = new Client();
+    try {
+        $json_res = $client->request('GET', "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/", ['query' => $params])->getBody();
+    } catch (Exception $e) {
+        return print("エラーが発生しました。");
     }
-
-    if (isset($response["results"]["shop"])) {
-        foreach ($response["shop"] as &$i) {
-            $restaurant_name = $i["name"];
-            $restaurants[] = $restaurant_name;
-        }
+    $response = json_decode($json_res, true);
+    
+    if (isset($response["results"]["error"])) {
+        return(print("エラーが発生しました！"));
     }
     
+    foreach ($response["results"]["shop"] as &$restaurant) {
+        $restaurants[] = $restaurant["name"];
+    }
+    $handle = fopen("restaurants_list.csv", "wb");
+    fputcsv($handle, $restaurants);
+    fclose($handle);
     return print_r($restaurants);
 }
 
-write_data_to_csv();
+write_data_to_csv($PARAMS);
